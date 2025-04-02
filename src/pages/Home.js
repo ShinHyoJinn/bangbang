b1e609720850f8d07a0434e599061f18fef7fb52
ShinHyoJinn <tlsgywls2003@naver.com>
2025-04-02 12:22:20 +0900

    처음 커밋

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import MainBackground from "../components/mainbackground";
 import Header, { Mainlogo } from "../components/Header"; 
 import MainImg1 from "../components/mainimg";
 import Menu from "../components/menu";
@@ -8,9 +7,8 @@ import Menu from "../components/menu";
 const Home = () => {
   return (
     <div>
-      <MainBackground />
-      <Header/>
       <MainImg1/>
+      <Header/>
       <Menu/>
     </div>
   );
